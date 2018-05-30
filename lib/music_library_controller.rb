1dac9d66c1ca6ca402cecd70a05186a577b38e5d
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-30 01:23:03 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,7 +15,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    user_input = ""
     user_input = gets until user_input == "exit"
   end
 
