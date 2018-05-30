81552bd17aa13d5df27daec9d4e3904a747f7910
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-30 02:12:38 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,7 +71,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
-    puts "Please enter the name of the genre:"
+    puts "Please enter the name of a genre:"
     genre_name = gets
 
   end
