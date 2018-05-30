5ef80522de5843182401924eff7d8b352d8b0e0e
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-30 02:41:04 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,14 @@ class MusicLibraryController
     user_input = gets
     if user_input == "list songs"
       self.list_songs
+    elsif user_input == "list artists"
+      self.list_artists
+    elsif user_input == "list genres"
+      self.list_genres
+    elsif user_input == "list artist"
+      self.list_songs_by_artist
+    elsif user_input == "list genre"
+      self.list_songs_by_genre
     end
     #until user_input == "exit"
   end
